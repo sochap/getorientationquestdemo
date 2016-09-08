400124b9e07de9590db48fd90da0e758c59b7160
so <so@so.com>
2016-09-08 16:19:40 +0800

    add getDefaultDisplay

--- app/src/main/java/com/app/my/myapplication/MainActivity.java
+++ app/src/main/java/com/app/my/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.OrientationEventListener;
 import android.view.Surface;
 import android.widget.Toast;
@@ -18,6 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.v("0","oncreate orientation="+getResources().getConfiguration().orientation); //this value is always 1
+
+
+        Display getOrient = getWindowManager().getDefaultDisplay();
+        int orientation = Configuration.ORIENTATION_UNDEFINED;
+        if(getOrient.getWidth()==getOrient.getHeight()){
+            orientation = Configuration.ORIENTATION_SQUARE;
+        } else{
+            if(getOrient.getWidth() < getOrient.getHeight()){
+                orientation = Configuration.ORIENTATION_PORTRAIT;
+            }else {
+                orientation = Configuration.ORIENTATION_LANDSCAPE;
+            }
+        }
+        Log.v("0","oncreate orientation from default display="+orientation); //this value is also always 1
 
 
     }
